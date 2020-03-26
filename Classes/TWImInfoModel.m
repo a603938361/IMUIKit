817a4b6c78bd71c8f3129fb33e7dda3a28fea02f
huangshunli <huangshunli@taiwu.com>
2020-03-26 09:40:39 +0800

    init

---
+++ Classes/TWImInfoModel.m
@@ -0,0 +1,13 @@
+//
+//  TWImInfoModel.m
+//  TaiWuOffice
+//
+//  Created by 田黎强 on 2020/2/21.
+//  Copyright © 2020 zsf. All rights reserved.
+//
+
+#import "TWImInfoModel.h"
+
+@implementation TWImInfoModel
+
+@end
